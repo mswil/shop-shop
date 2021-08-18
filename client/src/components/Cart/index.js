feeeee5b79e4ac01c74b281707f0abd57f6d30b1
Molly Wilson <molly_wilson1@outlook.com>
2021-08-18 16:36:12 -0500

    Add Stripe API to Process Payment

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -9,8 +9,24 @@ import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
 import Auth from '../../utils/auth';
 import { idbPromise } from "../../utils/helpers";
 
+import { QUERY_CHECKOUT } from '../../utils/queries';
+import { loadStripe } from '@stripe/stripe-js';
+import { useLazyQuery } from '@apollo/client';
+
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
+
 const Cart = () => {
     const [state, dispatch] = useStoreContext();
+
+    const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
+
+    useEffect(() => {
+        if (data) {
+            stripePromise.then((res) => {
+                res.redirectToCheckout({ sessionId: data.checkout.session });
+            });
+        }
+    }, [data]);
 
     useEffect(() => {
         async function getCart() {
@@ -45,7 +61,19 @@ const Cart = () => {
         );
     }
 
-    console.log(state)
+    function submitCheckout() {
+        const productIds = [];
+
+        state.cart.forEach((item) => {
+            for (let i = 0; i < item.purchaseQuantity; i++) {
+                productIds.push(item._id);
+            }
+        });
+
+        getCheckout({
+            variables: { products: productIds }
+        });
+    }
 
     return (
         <div className="cart">
@@ -60,7 +88,7 @@ const Cart = () => {
                         <strong>Total: ${calculateTotal()}</strong>
                         {
                             Auth.loggedIn() ?
-                                <button>
+                                <button onClick={submitCheckout}>
                                     Checkout
                                 </button>
                                 :
